0f66cd79d22fd69963673507b000acd6ce1eab90
liwenxing <13281060@bjtu.edu.cn>
2018-11-04 21:08:06 +0800

    2018-11-4 21:08:01

---
+++ src/main/java/com/liwx/algorithm/easy/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+package com.liwx.algorithm.easy;
+
+/**
+ * @author liwenxing
+ * @date 2018/11/4 20:23
+ *
+ * https://leetcode.com/problems/longest-common-prefix/
+ *
+ */
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        String result = "";
+        for (int i = 0; i < strs.length; i ++) {
+
+        }
+        return result;
+    }
+}
