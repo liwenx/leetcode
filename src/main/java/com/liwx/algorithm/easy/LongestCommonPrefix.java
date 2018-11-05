1b0e22db4e6e8fe7b38ad8a12244c3d275402733
liwenxing <13281060@bjtu.edu.cn>
2018-11-05 23:00:11 +0800

    2018-11-5 23:00:04

--- src/main/java/com/liwx/algorithm/easy/LongestCommonPrefix.java
+++ src/main/java/com/liwx/algorithm/easy/LongestCommonPrefix.java
@@ -1,5 +1,7 @@
 package com.liwx.algorithm.easy;
 
+import java.beans.beancontext.BeanContext;
+
 /**
  * @author liwenxing
  * @date 2018/11/4 20:23
@@ -9,14 +11,35 @@ package com.liwx.algorithm.easy;
  */
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
     }
 
     public static String longestCommonPrefix(String[] strs) {
-        String result = "";
-        for (int i = 0; i < strs.length; i ++) {
-
+        if (strs == null || strs.length == 0) {
+            return "";
         }
-        return result;
+        String minWord = strs[0];
+        for (int i = 0; i < strs.length; i ++) {
+            if (strs[i].length() < minWord.length()) {
+                minWord = strs[i];
+            }
+        }
+
+        while (minWord.length() > 0) {
+            boolean isTrue = true;
+            for (int i = 0; i < strs.length; i ++) {
+                if (!strs[i].startsWith(minWord)) {
+                    isTrue = false;
+                    break;
+                }
+            }
+            if (!isTrue) {
+                minWord = minWord.substring(0, minWord.length() - 1);
+            } else {
+                break;
+            }
+        }
+        return minWord;
     }
 }
