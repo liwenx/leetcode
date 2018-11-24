e4e926f5106595f27778913bc907dbf76e0fb8de
黎文兴 <liwenxing@hualala.com>
2018-11-24 15:50:06 +0800

    17

--- src/main/java/com/liwx/algorithm/medium/LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/com/liwx/algorithm/medium/LetterCombinationsOfAPhoneNumber.java
@@ -1,6 +1,6 @@
 package com.liwx.algorithm.medium;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -9,10 +9,21 @@ import java.util.List;
  */
 public class LetterCombinationsOfAPhoneNumber {
     public static void main(String[] args) {
-
+        System.out.println(letterCombinations("23"));
     }
+
     public static List<String> letterCombinations(String digits) {
-        List<String> resultList = new ArrayList<>();
-        return resultList;
+        LinkedList<String> ans = new LinkedList<String>();
+        if (digits.isEmpty()) return ans;
+        String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        ans.add("");
+        while (ans.peek().length() != digits.length()) {
+            String remove = ans.remove();
+            String map = mapping[digits.charAt(remove.length()) - '0'];
+            for (char c : map.toCharArray()) {
+                ans.addLast(remove + c);
+            }
+        }
+        return ans;
     }
 }
