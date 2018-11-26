57657fcdc6eff46b837431e47388f9c995dcde68
liwenxing <13281060@bjtu.edu.cn>
2018-11-26 23:24:30 +0800

    2018-11-26 23:24:14

--- src/main/java/com/liwx/algorithm/medium/RemoveNthNodeFromEndofList.java
+++ src/main/java/com/liwx/algorithm/medium/RemoveNthNodeFromEndofList.java
@@ -5,4 +5,33 @@ package com.liwx.algorithm.medium;
  * @date 2018/11/26 22:49
  */
 public class RemoveNthNodeFromEndofList {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode start = new ListNode(0);
+        ListNode slow = start, fast = start;
+        slow.next = head;
+        for (int i = 1; i <= n + 1; i ++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return start.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
 }
