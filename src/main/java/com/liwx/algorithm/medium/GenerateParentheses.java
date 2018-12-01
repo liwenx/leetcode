2de5e6f72a7b1c083a056eae5d08d289267bcb90
liwex <13281060@bjtu.edu.cn>
2018-12-02 01:33:23 +0800

    the 22th

--- src/main/java/com/liwx/algorithm/medium/GenerateParentheses.java
+++ src/main/java/com/liwx/algorithm/medium/GenerateParentheses.java
@@ -12,11 +12,32 @@ import java.util.List;
  */
 public class GenerateParentheses {
     public static void main(String[] args) {
-
+        System.out.println(generateParenthesis(3));
     }
-    public static List<String> generateParenthesis(int n) {
-        List<String> resList = new ArrayList<>();
 
-        return resList;
+    /**
+     * it's can work but i don't know why.
+     * @param n
+     * @return
+     */
+    public static List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        generateOneByOne("", list, n, n);
+        return list;
+    }
+    public static void generateOneByOne(String sublist, List<String> list, int left, int right){
+        if(left > right){
+            return;
+        }
+        if(left > 0){
+            generateOneByOne( sublist + "(" , list, left-1, right);
+        }
+        if(right > 0){
+            generateOneByOne( sublist + ")" , list, left, right-1);
+        }
+        if(left == 0 && right == 0){
+            list.add(sublist);
+            return;
+        }
     }
 }
