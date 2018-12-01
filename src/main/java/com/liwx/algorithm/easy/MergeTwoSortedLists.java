94a3950e31ae1710cce3a1a7e88680b31ef13d27
liwex <13281060@bjtu.edu.cn>
2018-12-01 22:12:01 +0800

    the 20th

--- src/main/java/com/liwx/algorithm/easy/MergeTwoSortedLists.java
+++ src/main/java/com/liwx/algorithm/easy/MergeTwoSortedLists.java
@@ -12,6 +12,12 @@ public class MergeTwoSortedLists {
 
     }
 
+    /**
+     * my answer
+     * @param l1
+     * @param l2
+     * @return
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode listNode = new ListNode(0);
         ListNode temp = listNode;
@@ -37,6 +43,30 @@ public class MergeTwoSortedLists {
         }
         temp.next = null;
         return listNode.next;
+    }
+
+    /**
+     * the one i think is very beautiful
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if( l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode newList;
+        if(l1.val < l2.val) {
+            newList = l1;
+            newList.next = mergeTwoLists2(l1.next, l2);
+        } else {
+            newList = l2;
+            newList.next = mergeTwoLists2(l1,l2.next);
+        }
+        return newList;
     }
 
     public class ListNode {
