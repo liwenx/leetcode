d9fed6448ba935b4fb3d3ffafe04bfef135fe16d
liwex <13281060@bjtu.edu.cn>
2018-12-02 01:10:15 +0800

    the 22th

---
+++ src/main/java/com/liwx/algorithm/medium/GenerateParentheses.java
@@ -0,0 +1,22 @@
+package com.liwx.algorithm.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * created by liwenxing.
+ * 2018-12-02-01-00
+ *
+ * https://leetcode.com/problems/generate-parentheses/
+ *
+ */
+public class GenerateParentheses {
+    public static void main(String[] args) {
+
+    }
+    public static List<String> generateParenthesis(int n) {
+        List<String> resList = new ArrayList<>();
+
+        return resList;
+    }
+}
