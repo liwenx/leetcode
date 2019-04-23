80d104bbb34b60bdf0b951265dcbffcb9c0965a3
liwenxing <liwenxing@xiaomi.com>
2019-04-23 11:17:48 +0800

    hello

---
+++ src/main/java/com/liwx/test/MiUbuntuTest.java
@@ -0,0 +1,12 @@
+package com.liwx.test;
+
+/**
+ * @author liwenxing
+ * @date 19-4-23 上午11:14
+ */
+public class MiUbuntuTest {
+    public static void main(String[] args) {
+        System.out.println("i am a mifans");
+    }
+}
+
