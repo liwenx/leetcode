e888efc4c97d7e5350fae908887505a7ddc18aa6
liwenxing <liwenxing@xiaomi.com>
2019-05-08 17:50:24 +0800

    2019-05-08 17:49:58

--- src/main/java/com/liwx/test/MiUbuntuTest.java
+++ src/main/java/com/liwx/test/MiUbuntuTest.java
@@ -8,6 +8,7 @@ public class MiUbuntuTest {
     public static void main(String[] args) {
         System.out.println("i am a mifans");
         System.out.println("The second push by mi ubuntu.");
+        System.out.println(" merge test");
     }
 }
 
