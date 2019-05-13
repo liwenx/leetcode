ca78a3c53405fc19a839ec31d151a458a72eab2f
liwenxing <13281060@bjtu.edu.cn>
2019-05-13 23:42:16 +0800

    2019-5-13 23:42:09

--- src/main/java/com/liwx/algorithm/hard/SubstringWithConcatenationOfAllWords.java
+++ src/main/java/com/liwx/algorithm/hard/SubstringWithConcatenationOfAllWords.java
@@ -1,26 +1,23 @@
 package com.liwx.algorithm.hard;
 
+import java.util.List;
+
 /**
  * @author liwenxing
  * @date 2019/1/14 18:34
- *
+ * <p>
  * https://leetcode.com/problems/substring-with-concatenation-of-all-words/
- *
  */
 public class SubstringWithConcatenationOfAllWords {
-    static int a = 0;
     public static void main(String[] args) {
-    foo(9);
-        System.out.println(a);
+
     }
 
-    public static int foo (int i) {
-        a =a+1;
-        if (i < 2) {
-            return 1;
-        } else {
-            return foo(i-3) + foo(i-2) + 1;
-        }
+    public static List<Integer> findSubstring(String s, String[] words) {
+
+
+        return null;
     }
+
 
 }
