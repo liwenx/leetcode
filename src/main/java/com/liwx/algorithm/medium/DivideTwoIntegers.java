5468a5f6d70ef149e1434dcf994a4b15d1d90d6b
liwenxing <13281060@bjtu.edu.cn>
2019-05-13 23:38:01 +0800

    2019-5-13 23:37:41

--- src/main/java/com/liwx/algorithm/medium/DivideTwoIntegers.java
+++ src/main/java/com/liwx/algorithm/medium/DivideTwoIntegers.java
@@ -3,22 +3,29 @@ package com.liwx.algorithm.medium;
 /**
  * @author liwenxing
  * @date 19-5-13 下午8:44
- *
+ * <p>
  * https://leetcode.com/problems/divide-two-integers/
- *
  */
 public class DivideTwoIntegers {
     public static void main(String[] args) {
-
+        System.out.println(Math.abs(Integer.MIN_VALUE));
+//        System.out.println(divide(Integer.MIN_VALUE, 1));
     }
 
+    /**
+     * 会 Time Limit Exceeded
+     *
+     * @param dividend
+     * @param divisor
+     * @return
+     */
     public static int divide(int dividend, int divisor) {
         //只有此条件下会溢出
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
-        long dvd = Math.abs(dividend), dvs = Math.abs(divisor);
-        int answer = 0;
+        long dvd = Math.abs((long) dividend), dvs = Math.abs(divisor);
+        long answer = 0;
         int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
         while (dvd >= dvs) {
             long temp = dvs, m = 1;
@@ -29,6 +36,40 @@ public class DivideTwoIntegers {
             dvd -= temp;
             answer += m;
         }
-        return sign * answer;
+        return (int) (sign * answer);
     }
+
+    public static int divide2(int dividend, int divisor) {
+        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        long ldividend = Math.abs((long) dividend);
+        long ldivisor = Math.abs((long) divisor);
+        if (ldivisor == 0) {
+            return Integer.MAX_VALUE;
+        }
+        if (ldividend == 0 || ldividend < ldivisor) {
+            return 0;
+        }
+        long lans = ldivide(ldividend, ldivisor);
+        int ans;
+        if (lans > Integer.MAX_VALUE) {
+            ans = (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        } else {
+            ans = (int) (sign * lans);
+        }
+        return ans;
+    }
+
+    private static long ldivide(long ldividend, long ldivisor) {
+        if (ldividend < ldivisor) {
+            return 0;
+        }
+        long sum = ldivisor;
+        long multiple = 1;
+        while ((sum + sum) <= ldividend) {
+            sum += sum;
+            multiple += multiple;
+        }
+        return multiple + ldivide(ldividend - sum, ldivisor);
+    }
+
 }
