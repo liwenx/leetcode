c589fa64ef0695339acb8b610f20205cc7ea2c24
liwenxing <liwenxing@xiaomi.com>
2019-05-15 14:36:49 +0800

    commit test

---
+++ src/main/java/com/liwx/algorithm/medium/NextPermutation.java
@@ -0,0 +1,18 @@
+package com.liwx.algorithm.medium;
+
+/**
+ * @author liwenxing
+ * @date 19-5-14 下午8:01
+ *
+ * https://leetcode.com/problems/next-permutation/
+ *
+ */
+public class NextPermutation {
+    public static void main(String[] args) {
+
+    }
+
+    public static void nextPermutation(int[] nums) {
+
+    }
+}
