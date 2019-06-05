2aa2bc9f8aa1426959289db08c98270bbce85adb
liwenxing <liwenxing@xiaomi.com>
2019-06-05 20:19:21 +0800

    37

---
+++ src/main/java/com/liwx/algorithm/hard/SudokuSolver.java
@@ -0,0 +1,19 @@
+package com.liwx.algorithm.hard;
+
+/**
+ *
+ *
+ * https://leetcode.com/problems/sudoku-solver/
+ *
+ * @author liwenxing
+ * @date 19-6-5 下午8:17
+ */
+public class SudokuSolver {
+    public static void main(String[] args) {
+
+    }
+
+    public static void solveSudoku(char[][] board) {
+
+    }
+}
