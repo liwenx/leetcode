737e13ab3de499d046c87cea606483c67feebddd
liwenxing <liwenxing@xiaomi.com>
2019-06-25 20:50:21 +0800

    2019-06-25 20:50:20

--- src/main/java/com/liwx/algorithm/medium/CombinationSum.java
+++ src/main/java/com/liwx/algorithm/medium/CombinationSum.java
@@ -11,7 +11,8 @@ import java.util.List;
  */
 public class CombinationSum {
     public static void main(String[] args) {
-
+        int[] candidates = {2,3,6,7};
+        System.out.println(combinationSum(candidates, 7));
     }
 
     /**
@@ -31,10 +32,7 @@ public class CombinationSum {
         if (remain < 0) return;
         else if (remain == 0) list.add(new ArrayList<>(tempList));
         else {
-            for (int i = 0; i < nums.length; i++) {
-                if (i > start && nums[i] == nums[i-1]) {
-                    continue;
-                }
+            for (int i = start; i < nums.length; i++) {
                 tempList.add(nums[i]);
                 backtrack(list, tempList, nums, remain - nums[i], i);
                 tempList.remove(tempList.size() - 1);
