7017839a35dc1b0af1dbebafd4598e16966b883f
liwenxing <liwenxing@xiaomi.com>
2019-07-17 20:35:02 +0800

    2019-07-17 20:34:56

---
+++ src/main/java/com/liwx/algorithm/medium/MultiplyStrings.java
@@ -0,0 +1,25 @@
+package com.liwx.algorithm.medium;
+
+/**
+ *
+ * https://leetcode.com/problems/multiply-strings/
+ *
+ * @author liwenxing
+ * @date 7/17/19 8:20 PM
+ */
+public class MultiplyStrings {
+    public static void main(String[] args) {
+
+    }
+    public static String multiply(String num1, String num2) {
+        char[] arr1 = num1.toCharArray();
+        char[] arr2 = num2.toCharArray();
+
+        for (int i = 0; i < arr1.length; i++) {
+
+        }
+
+        return null;
+    }
+
+}
