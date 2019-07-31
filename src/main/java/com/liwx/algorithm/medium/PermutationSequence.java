bb8c0526a6064160bbe6fa44c8e23b1cfb11513c
liwenxing <liwenxing@xiaomi.com>
2019-07-31 10:28:09 +0800

    60

---
+++ src/main/java/com/liwx/algorithm/medium/PermutationSequence.java
@@ -0,0 +1,19 @@
+package com.liwx.algorithm.medium;
+
+/**
+ *
+ * https://leetcode.com/problems/permutation-sequence/
+ *
+ * @author liwenxing
+ * @date 7/30/19 9:35 PM
+ */
+public class PermutationSequence {
+    public static void main(String[] args) {
+
+    }
+
+    public String getPermutation(int n, int k) {
+
+        return null;
+    }
+}
