7c1ba5943d9ce1314fa607701ccd0b8536b82932
liwenxing <liwenxing@xiaomi.com>
2019-08-19 20:58:27 +0800

    tree

--- src/main/java/com/liwx/algorithm/common/tree/BinaryTreeTraversal.java
+++ src/main/java/com/liwx/algorithm/common/tree/BinaryTreeTraversal.java
@@ -1,5 +1,9 @@
 package com.liwx.algorithm.common.tree;
 
+import com.liwx.algorithm.util.TreeNode;
+
+import java.util.Stack;
+
 /**
  *
  * 遍历二叉树
@@ -8,4 +12,25 @@ package com.liwx.algorithm.common.tree;
  * @date 8/19/19 8:11 PM
  */
 public class BinaryTreeTraversal {
+
+    /**
+     * 递归先序遍历
+     */
+    public static void recursionPreorderTraversal(TreeNode root) {
+        if (root != null) {
+            System.out.println(root.val + " ");
+            recursionPreorderTraversal(root.left);
+            recursionPreorderTraversal(root.right);
+        }
+    }
+
+    /**
+     * 非递归先序遍历
+     * @param root
+     */
+    public static void preorderTraversal(TreeNode root) {
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+
+    }
+
 }
