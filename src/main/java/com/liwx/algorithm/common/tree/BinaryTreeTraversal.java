ba91c0597e27124fb196a941833715c3b17355d2
liwenxing <liwenxing@xiaomi.com>
2019-08-20 20:17:43 +0800

    tree

--- src/main/java/com/liwx/algorithm/common/tree/BinaryTreeTraversal.java
+++ src/main/java/com/liwx/algorithm/common/tree/BinaryTreeTraversal.java
@@ -14,6 +14,10 @@ import java.util.Stack;
 public class BinaryTreeTraversal {
 
     /**
+     * 参考：1.https://www.jianshu.com/p/456af5480cee
+     *
+     */
+    /**
      * 递归先序遍历
      */
     public static void recursionPreorderTraversal(TreeNode root) {
@@ -29,7 +33,29 @@ public class BinaryTreeTraversal {
      * @param root
      */
     public static void preorderTraversal(TreeNode root) {
+        // 用来暂存节点的栈
         Stack<TreeNode> treeNodeStack = new Stack<>();
+        // 新建一个游标节点为根节点
+        TreeNode node = root;
+        // 当遍历到最后一个节点的时候，无论它的左右子树都为空，并且栈也为空
+        // 所以，只要不同时满足这两点，都需要进入循环
+        while (node != null || !treeNodeStack.isEmpty()) {
+            // 若当前考查节点非空，则输出该节点的值
+            // 由考查顺序得知，需要一直往左走
+            while (node != null) {
+                // 为了之后能找到该节点的右子树，暂存该节点
+                System.out.println(node.val + " ");
+                treeNodeStack.push(node);
+                node = node.left;
+            }
+            // 一直到左子树为空，则开始考虑右子树
+            // 如果栈已空，就不需要再考虑
+            // 弹出栈顶元素，将游标等于该节点的右子树
+            if (!treeNodeStack.isEmpty()) {
+                node = treeNodeStack.pop();
+                node = node.right;
+            }
+        }
 
     }
 
