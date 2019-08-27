a0cd4af24565fe977d18963777826993cbb8f282
liwenxing <liwenxing@xiaomi.com>
2019-08-27 20:22:48 +0800

    tree

--- src/main/java/com/liwx/algorithm/common/tree/BinaryTreeTraversal.java
+++ src/main/java/com/liwx/algorithm/common/tree/BinaryTreeTraversal.java
@@ -8,6 +8,12 @@ import java.util.Stack;
  *
  * 遍历二叉树
  *
+ * 先序：考察到一个节点后，即刻输出该节点的值，并继续遍历其左右子树。(根左右)
+ *
+ * 中序：考察到一个节点后，将其暂存，遍历完左子树后，再输出该节点的值，然后遍历右子树。(左根右)
+ *
+ * 后序：考察到一个节点后，将其暂存，遍历完左右子树后，再输出该节点的值。(左右根)
+ *
  * @author liwenxing
  * @date 8/19/19 8:11 PM
  */
@@ -56,7 +62,78 @@ public class BinaryTreeTraversal {
                 node = node.right;
             }
         }
+    }
 
+    /**
+     * 递归中序遍历
+     */
+    public static void recursionMiddleorderTraversal(TreeNode root) {
+        if (root != null) {
+            recursionMiddleorderTraversal(root.left);
+            System.out.println(root.val + " ");
+            recursionMiddleorderTraversal(root.right);
+        }
+    }
+    /**
+     * 非递归中序遍历
+     */
+    public static void middleorderTraversal(TreeNode root) {
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+        TreeNode node = root;
+        while (node!= null || !treeNodeStack.isEmpty()) {
+            while (node != null) {
+                treeNodeStack.push(node);
+                node = node.left;
+            }
+            if (!treeNodeStack.isEmpty()) {
+                node = treeNodeStack.pop();
+                System.out.println(node.val + " ");
+                node = node.right;
+            }
+        }
+    }
+
+    /**
+     * 递归后序遍历
+     */
+    public static void recursionPostorderTraversal(TreeNode root) {
+        if (root != null) {
+            recursionPostorderTraversal(root.left);
+            recursionPostorderTraversal(root.right);
+            System.out.println(root.val + " ");
+        }
+    }
+
+    /**
+     * 非递归后序遍历
+     *
+     * 后序遍历在决定是否可以输出当前节点的值的时候，需要考虑其左右子树是否都已经遍历完成。
+     *
+     * @param root
+     */
+    public static void postorderTraversal(TreeNode root) {
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+        TreeNode node = root;
+        TreeNode lastVisit = root;
+        while (node != null || !treeNodeStack.isEmpty()) {
+            while (node != null) {
+                treeNodeStack.push(node);
+                node = node.left;
+            }
+            //查看当前栈顶元素
+            node = treeNodeStack.peek();
+            //如果其右子树也为空，或者右子树已经访问
+            //则可以直接输出当前节点的值
+            if (node.right == null || node.right == lastVisit) {
+                System.out.println(node.val + " ");
+                treeNodeStack.pop();
+                lastVisit = node;
+                node = null;
+            } else {
+                //否则，继续遍历右子树
+                node = node.right;
+            }
+        }
     }
 
 }
