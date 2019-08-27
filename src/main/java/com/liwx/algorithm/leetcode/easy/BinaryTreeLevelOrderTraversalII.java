e4a658730c9668617a23b049a672c945b5193ba8
liwenxing <liwenxing@xiaomi.com>
2019-08-27 21:43:30 +0800

    empty class

---
+++ src/main/java/com/liwx/algorithm/leetcode/easy/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,25 @@
+package com.liwx.algorithm.leetcode.easy;
+
+import com.liwx.algorithm.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+ *
+ * @author liwenxing
+ * @date 8/27/19 9:39 PM
+ */
+public class BinaryTreeLevelOrderTraversalII {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> resultList = new ArrayList<>();
+
+        return resultList;
+    }
+
+}
