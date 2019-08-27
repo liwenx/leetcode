040f0eddf1c15727c694232b41ed1fdae98a2900
liwenxing <liwenxing@xiaomi.com>
2019-08-27 21:34:42 +0800

    empty class

---
+++ src/main/java/com/liwx/algorithm/leetcode/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,23 @@
+package com.liwx.algorithm.leetcode.medium;
+
+import com.liwx.algorithm.util.TreeNode;
+
+/**
+ *
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ *
+ * @author liwenxing
+ * @date 8/27/19 9:12 PM
+ */
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+
+        return null;
+    }
+
+}
