690b128361bc3a415a0ccb94f5bb952e2d859e61
liwenxing <liwenxing@xiaomi.com>
2019-09-17 21:15:19 +0800

    107

--- src/main/java/com/liwx/algorithm/leetcode/easy/BinaryTreeLevelOrderTraversalII.java
+++ src/main/java/com/liwx/algorithm/leetcode/easy/BinaryTreeLevelOrderTraversalII.java
@@ -3,6 +3,7 @@ package com.liwx.algorithm.leetcode.easy;
 import com.liwx.algorithm.util.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -16,10 +17,27 @@ public class BinaryTreeLevelOrderTraversalII {
     public static void main(String[] args) {
 
     }
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> resultList = new ArrayList<>();
 
+    /**
+     * BFS solution
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> resultList = new LinkedList<>();
+        levelMaker(resultList, root, 0);
         return resultList;
+    }
+    public void levelMaker(List<List<Integer>> list, TreeNode root,  int level) {
+        if (root == null) {
+            return;
+        }
+        if (level >= list.size()) {
+            list.add(0, new LinkedList<>());
+        }
+        levelMaker(list, root.left, level+1);
+        levelMaker(list, root.right, level+1);
+        list.get(list.size()-level-1).add(root.val);
     }
 
 }
