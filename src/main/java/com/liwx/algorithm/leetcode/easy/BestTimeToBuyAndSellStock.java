25732301b676505dcfea76fb45855e972f345fc8
liwenxing <liwenxing@xiaomi.com>
2019-12-10 20:52:09 +0800

    121

--- src/main/java/com/liwx/algorithm/leetcode/easy/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/liwx/algorithm/leetcode/easy/BestTimeToBuyAndSellStock.java
@@ -26,4 +26,23 @@ public class BestTimeToBuyAndSellStock {
         }
         return max;
     }
+
+    /**
+     * DP思想
+     * 1.记录[今天之前买入的最小值]
+     * 2.计算【今天之前最小值买入，今天卖出的获利】，也即【今天卖出的最大获利】
+     * 3.比较【每天的最大获利】，取最大值即可
+     */
+    public int maxProfit2(int[] prices) {
+        if (prices.length <= 1) {
+            return 0;
+        }
+
+        int min = prices[0], max = 0;
+        for (int i = 1; i < prices.length; i++) {
+            max = Math.max(max, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return max;
+    }
 }
