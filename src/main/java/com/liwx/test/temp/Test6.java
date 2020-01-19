b0555874cccd5588c54a955e3a3bbe07cd3a9812
liwenxing <liwenxing@xiaomi.com>
2020-01-19 19:09:47 +0800

    111

---
+++ src/main/java/com/liwx/test/temp/Test6.java
@@ -0,0 +1,13 @@
+package com.liwx.test.temp;
+
+import java.time.LocalDate;
+
+/**
+ * @author liwenxing
+ * @date 2020/1/6 下午3:51
+ */
+public class Test6 {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now().toString());
+    }
+}
