0b5ec577bbb3172fbc80f7af90da693dfeec42b5
liwenxing <liwenxing@xiaomi.com>
2021-04-05 15:02:29 +0800

    147

---
+++ src/main/java/com/liwx/algorithm/leetcode/medium/InsertionSortList.java
@@ -0,0 +1,37 @@
+package com.liwx.algorithm.leetcode.medium;
+
+import com.liwx.algorithm.util.ListNode;
+
+/**
+ *
+ * https://leetcode-cn.com/problems/insertion-sort-list/
+ *
+ * @author liwenxing
+ * @date 2021/4/5 下午2:32
+ */
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val <= curr.val) {
+                    prev = prev.next;
+                }
+                //移动到此处
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+    }
+}
