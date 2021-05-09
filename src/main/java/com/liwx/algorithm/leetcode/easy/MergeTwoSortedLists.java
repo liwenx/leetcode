673b2f843273c7927b3944c8e2253f9051a2a4cd
liwenxing <liwenxing@xiaomi.com>
2021-05-09 16:22:06 +0800

    合并有序链表

--- src/main/java/com/liwx/algorithm/leetcode/easy/MergeTwoSortedLists.java
+++ src/main/java/com/liwx/algorithm/leetcode/easy/MergeTwoSortedLists.java
@@ -16,10 +16,12 @@ public class MergeTwoSortedLists {
 
     /**
      * my answer
+     * 不对，创建新节点了
      * @param l1
      * @param l2
      * @return
      */
+    @Deprecated
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode listNode = new ListNode(0);
         ListNode temp = listNode;
@@ -46,6 +48,31 @@ public class MergeTwoSortedLists {
         temp.next = null;
         return listNode.next;
     }
+
+    /**
+     * 这个答案才是对的！！！！！！！！！！
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoListsRight(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode(0);
+        ListNode temp = listNode;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        temp.next = l1 == null ? l2 : l1;
+        return listNode.next;
+    }
+
 
     /**
      * the one i think is very beautiful
